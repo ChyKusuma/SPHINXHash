cc47bc07f6695a3ae9d274a2d47b3f3452d6b43e
Kusuma <119119194+ChyKusuma@users.noreply.github.com>
2023-07-15 19:35:14 +0700

    Update Hash.hpp
    Update the header into sha3.h instead swifftx.h
    
    Signed-off-by: Kusuma <119119194+ChyKusuma@users.noreply.github.com>

--- Hash.hpp
+++ Hash.hpp
@@ -11,26 +11,6 @@
  */
 
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-// The provided code implements a SWIFFTX 256 digest size hash function for calculating the hash of a message.
-
-// The SPHINX_256 function takes a message as input and calculates its SWIFFTX-256 hash using the SWIFFTX library. Here's how the process works:
-
-// A hashState object state and a BitSequence array hashval of size 32 (corresponding to the 256-bit hash output) are declared.
-  // The Init function is called to initialize the state object with a hash bit length of 256.
-
-  // The Update function is called to process the message. The message is converted to a BitSequence array and its length is multiplied by 8 to convert it to bits. The Update function updates the state object with the message data.
-
-  // The Final function is called to finalize the hash calculation. The resulting hash value is stored in the hashval array.
-
-  // The hash value is then converted to a hexadecimal string representation. This is done by iterating over each element in the hashval array, converting it to a two-digit hexadecimal representation using the sprintf function, and appending it to the result string.
-
-  // The result string, containing the hexadecimal hash value, is returned as the output of the sha3_256 function.
-
-// Note: that the code provided is a simplified version and may require additional implementation details for error handling, memory management, and other functionalities related to using the SWIFFTX library and calculating SSPHINX-256 hashes.
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
 
 #ifndef HASH_HPP
 #define HASH_HPP
@@ -40,29 +20,53 @@
 #include <string>
 #include <vector>
 
-#include "Lib/Swifftx/stdint.h"
-#include "Lib/swifftx/SWIFFTX.h"
+#include "lib/Swifftx/SHA3.h"
 
 
 namespace SPHINXHash {
 
 std::string SPHINX_256(const std::string& message) {
-    swifftx::hash::SWIFFTX256 hasher;
-    swifftx::hash::digest256_t digest;
+    hashState state;
+    BitSequence hashval[SWIFFTX_OUTPUT_BLOCK_SIZE];
+      // These lines declare variables state of type hashState and hashval as an array of BitSequence 
+      // with a size of SWIFFTX_OUTPUT_BLOCK_SIZE. These variables will be used to store the hash state 
+      // and the resulting hash value, respectively.
+    
+    // Initialize the hash state with the intended hash length
+    Init(&state, 256); // Replace "256" with the desired hash length
 
-    hasher.init();
-    hasher.update(reinterpret_cast<const uint8_t*>(message.c_str()), message.length());
-    hasher.final(digest);
+      // This line initializes the state variable by calling the Init function from the SWIFFTX library. 
+      // The second argument 256 specifies the desired hash length in bits. 
+      // IT can replace 256 with the desired hash length.
 
-    // Convert the digest value to a hexadecimal string
+    // Update the hash state with the message
+    Update(&state, reinterpret_cast<const BitSequence*>(message.c_str()), message.length() * 8);
+
+      // This line updates the hash state by calling the Update function from the SWIFFTX library. 
+      // It takes the state variable, casts the message string to a const BitSequence*, and provides 
+      // the length of the message in bits. This step processes the input message and updates the hash
+      // state accordingly.
+
+    // Finalize the hash computation and obtain the hash value
+    Final(&state, hashval);
+
+      // This line finalizes the hash computation by calling the Final function from the SWIFFTX library.
+      // It takes the state variable and the hashval array as arguments. This step computes the final hash
+      // value based on the processed input message and stores it in the hashval array.
+
+    // Convert the hash value to a hexadecimal string
     std::string result;
-    for (int i = 0; i < 32; ++i) {
+    for (int i = 0; i < SWIFFTX_OUTPUT_BLOCK_SIZE; ++i) {
         char hex[3];
-        sprintf(hex, "%02x", digest[i]);
+        sprintf(hex, "%02x", hashval[i]);
         result += hex;
     }
+      // These lines convert the hashval array to a hexadecimal string representation. It iterates over
+      // each element of the hashval array, converts it to a two-digit hexadecimal representation using
+      // sprintf, and appends it to the result string.
 
     return result;
+      // This line returns the resulting hash value as a hexadecimal string.
 }
 
 } // namespace SPHINXHash
